a1ad3ac5b4eac6b96e230c2a2d559531e5df8f8e
AbilashBodapati <43051563+AbilashBodapati@users.noreply.github.com>
2019-02-23 11:22:41 -0500

    Passed info into setInfo method. created setInfo and getInfo methods.

--- TestFolder/ProductTest.java
+++ TestFolder/ProductTest.java
@@ -8,10 +8,62 @@
 public class ProductTest {
 
     /**
+     * Method to return the set Product.
+     *
+     * @param name
+     * @param type
+     * @param price
+     * @param quantity
+     * @param inStock
+     * @return {@code sampleProduct}
+     */
+    public static Product setInfo(String name, String type, double price,
+            int quantity, boolean inStock) {
+
+        Product sampleProduct = new SimpleProduct();
+
+        return sampleProduct;
+
+    }
+
+    /**
+     *
+     *
+     * @param sampleProduct
+     */
+    public static void getInfo(Product sampleProduct) {
+
+    }
+
+    /**
+     * Main Method
+     *
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+
+        /*
+         * We are going to Test the Accessors and Mutators of SimpleProduct
+         * class.
+         */
+        /*
+         * Sample product information.
+         *
+         * "The Shawshank Redemption", "DVD", "19.95", "100", "true"
+         */
+
+        // Sample Information
+        String name = "The Shawshank Redemption";
+        String type = "DVD";
+        double price = 19.95;
+        int quantity = 100;
+        boolean inStock = true;
+
+        // Setting information
+        Product sampleProduct = setInfo(name, type, price, quantity, inStock);
+
+        // Getting information
+        getInfo(sampleProduct);
 
     }
 
